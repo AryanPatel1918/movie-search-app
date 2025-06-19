ef73cfc2ca38488d6de6b4cce8b58e745c1a1368
aryanpatel1918 <aryanpatel1918@gmail.com>
2025-06-19 15:34:47 -0400

    Complete app

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,27 @@
+import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCard"
+
 export default function Favorites() {
+    const {favorites} = useMovieContext()
+
     return (
-        <div className="favorites-empty">
-            <h2>No favorite Movies Yet</h2>
-            <p>Start adding movies to your favorites</p>
-        </div>
+        <>
+            {favorites.length > 0 ? (
+                <div className="favorites">
+                    <h2>Your Favorites</h2>
+                    <div className="movies-grid">
+                        {favorites.map((movie) => (
+                            <MovieCard movie={movie} key={movie.id} />
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                <div className="favorites-empty">
+                    <h2>No Favorite Movies Yet</h2>
+                    <p>Start adding movies to your favorites</p>
+                </div>
+            )}
+        </>
     )
 }
